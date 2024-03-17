01d98c02f233954193bd81c55c2464d16924773e
Tof <tof.aliev@ru.axept.com>
2024-03-17 22:03:12 +0500

    First commit

---
+++ lesson.py
@@ -0,0 +1,37 @@
+"""
+GIT is the main logic
+GITHUB is a GUI - Graphical User Interface
+
+# Terminal Commands
+git clone <https/ssh> - cloning remote git repository to local machine
+ls(list) - list all files and directories from current path
+cd(change directory) <path> - change directory to selected path
+(VS Code) - > code <path> - opens visual studio code form current folder
+
+(Pyhcharm):
+    1. Windows:
+        a. Open Pycharm -> Tools -> Create Command-Line Launcher
+        b. You will see notification window and copy highlited path
+        c. Open Windows start menu and start typing "environment variables"
+        d. Select "Edit the System Environment Variables"
+        e. On the popup click on the button "Environment Variables..."
+        f. Under the System Variables section find the Path Variable
+        g. Click the button "Edit"
+        h. Add a New entry with a value of the copied Path
+        k. Save settings and close all windows
+
+    2. MacOS:
+        Open ToolBox
+        Choose PyCharm
+        Go to Settings
+        Open Configuration
+        Select Shell script name
+        And give what ever name
+        Run this name in command line
+
+git status - returns information about current repository(staged, to commit, untracked files)
+
+git add <path to the repo. file> - adds a path to current commit
+git remote -v -> returns list of remote git repository connections, origin is default remote name created by git
+git branch - returns list of repo branchesg
+"""
